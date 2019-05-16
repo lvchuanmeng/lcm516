1dfa323ac20d1ad479a2aa2e132d2283c039d789
lvchuanmeng <17600580217@163.com>
2019-05-16 21:11:16 +0800

    第一次提交1.php 和 2.php

---
+++ 1.php
@@ -0,0 +1,22 @@
+<?php
+// $a="hello";
+// $a="world";
+// echo "<a href='2.php?m=$a'>点我跳到2.php</a>";
+
+// echo
+// "
+// <form name='form1' method='post' action='2.php'>
+// <input type='text' name='val' />
+// <input type='submit' name='submit' value='提交' />
+// </form>
+
+// ";
+// session_start();
+// $_SESSION['val']=123;
+// echo $_SESSION['val'];
+// setcookie("user", "LCM" , time()+3600);
+// echo $_COOKIE["user"];
+
+// $str="asdfhdjsijbisdbdbkbkdjbijfbjdfbkjbkjbkbkbkccvccv";
+
+// echo strlen($str) <= 25 ? $str : substr($str, 0,25)."...";
---
+++ 2.php
@@ -0,0 +1,8 @@
+<?php
+// require_once("./1.php");
+// echo $a." world! ";
+// echo "hello".$_GET['m'];
+// echo $_POST['val'];
+// session_start();
+// echo $_SESSION['val'];
+echo $_COOKIE["user"];
